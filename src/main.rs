1a4f98ed46cfb386556020e904535ced4676616e
lee <leeian1011@gmail.com>
2024-03-29 23:00:15 +0800

    feat: expanded on help command

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,40 @@ fn main() {
 
     let mut stdin_handle = std::io::stdin().lock();  
     while stdin_handle.read_line(&mut stdin_buffer).unwrap() > 0 {
-        match stdin_buffer.trim_end() {
+        let split_buffer = stdin_buffer.split(' ').collect::<Vec<_>>();
+        match split_buffer.first().unwrap().trim() {
+            "list" => {
+                
+            }
             "help" => {
-                Printer::println_colour("todors: You may run the following commands", &TODORSCOLOURS);
-                Printer::box_print(&HELP_LIST, &TODORSCOLOURS);
+                if split_buffer.len() == 1 {
+                    Printer::println_colour("todors: You may run the following commands", &TODORSCOLOURS);
+                    Printer::box_print(&HELP_LIST, &TODORSCOLOURS);
+                } else if split_buffer.len() == 2 {
+                    match split_buffer[1].trim() {
+                        "list" => {
+                            todo!("how ironic");
+                        },
+                        "add" => {
+                            todo!("how ironic");
+                        },
+                        "remove" => {
+                            todo!("how ironic");
+                        },
+                        "complete" => {
+                            todo!("how ironic");
+                        }
+                        _ => {
+                            Printer::println_colour("todors: I didn't quite get that", &TODORSCOLOURS);
+                            Printer::println_colour("todors: You may run the following commands", &TODORSCOLOURS);
+                            Printer::box_print(&HELP_LIST, &TODORSCOLOURS);
+                        }
+                    }
+                } else {
+                    Printer::println_colour("todors: I didn't quite get that", &TODORSCOLOURS);
+                    Printer::println_colour("todors: You may run the following commands", &TODORSCOLOURS);
+                    Printer::box_print(&HELP_LIST, &TODORSCOLOURS);
+                }
             }
             "exit" => {
                 Printer::println_colour("todors: goodbye!", &TODORSCOLOURS);
