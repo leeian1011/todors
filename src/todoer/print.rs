187217bf2706184f100ccf275207535432ffda8c
lee <leeian1011@gmail.com>
2024-03-29 22:44:54 +0800

    feat: introduce Printer for ascii-box generation

---
+++ src/todoer/print.rs
@@ -0,0 +1,129 @@
+pub const RED_TEXT: &'static str = "\u{001b}[31m";
+pub const GREEN_TEXT: &'static str = "\u{001b}[32m";
+pub const BLUE_TEXT: &'static str = "\u{001b}[34m";
+pub const RED_BG: &'static str = "\u{001b}[41m";
+pub const GREEN_BG: &'static str = "\u{001b}[42m";
+pub const BLUE_BG: &'static str = "\u{001b}[44m";
+pub const RESET: &'static str = "\u{001b}[0m";
+
+const LINECOLOURS: [Colour;1] = [Colour::BlueText];
+
+#[allow(dead_code)]
+pub enum Colour {
+    RedText,
+    GreenText,
+    BlueText,
+    RedBg,
+    GreenBg,
+    BlueBg,
+}
+
+fn get_colour_code(colour: &Colour) -> &'static str {
+    match colour {
+        Colour::RedText => RED_TEXT,
+        Colour::GreenText => GREEN_TEXT,
+        Colour::BlueText => BLUE_TEXT,
+        Colour::RedBg => RED_BG,
+        Colour::GreenBg => GREEN_BG,
+        Colour::BlueBg => BLUE_BG,
+    }
+}
+
+pub struct Printer;
+
+impl Printer {
+    fn gen_line(length: usize) {
+        print_colour("+", &LINECOLOURS);
+        for _ in 0..length {
+            print_colour("=", &LINECOLOURS);
+        }
+        println_colour("+", &LINECOLOURS);
+    }
+
+    fn gen_segment(subject: &str, colours: &[Colour]) {
+        print_colour("|", &LINECOLOURS);
+        print_colour(subject, colours);
+    }
+
+    fn add_padding(max_length: usize, text_length: usize) {
+        let length = max_length - text_length;
+        for _ in 0..length {
+            print!(" ");
+        }
+        println_colour("|", &LINECOLOURS);
+    }
+
+    pub fn box_print(list: &[&str], colours: &[Colour]) {
+        let mut longest = 0;
+        for item in list {
+            if item.len() > longest {
+                longest = item.len();
+            }
+        }
+        Printer::gen_line(longest);
+        for item in list {
+            Printer::gen_segment(item, colours);
+            Printer::add_padding(longest, item.len());
+        }
+        Printer::gen_line(longest);
+    }
+}
+
+
+pub fn println_colour(subject: &str, colours: &[Colour]) {
+    let codes = colours
+        .iter()
+        .map(|colour| get_colour_code(colour))
+        .collect::<Vec<_>>();
+    let mut outcome = String::new();
+    for code in codes {
+        outcome.push_str(code);
+    }
+
+    outcome += &subject.to_string();
+    outcome.push_str(RESET);
+    println!("{}", outcome);
+}
+
+pub fn print_colour(subject: &str, colours: &[Colour]) {
+    let codes = colours
+        .iter()
+        .map(|colour| get_colour_code(colour))
+        .collect::<Vec<_>>();
+    let mut outcome = String::new();
+    for code in codes {
+        outcome.push_str(code);
+    }
+
+    outcome += &subject.to_string();
+    outcome.push_str(RESET);
+    print!("{}", outcome);
+}
+
+pub fn print_colour_no_reset(subject: &str, colours: &[Colour]) {
+    let codes = colours
+        .iter()
+        .map(|colour| get_colour_code(colour))
+        .collect::<Vec<_>>();
+    let mut outcome = String::new();
+    for code in codes {
+        outcome.push_str(code);
+    }
+
+    outcome += &subject.to_string();
+    print!("{}", outcome);
+}
+
+pub fn println_colour_no_reset(subject: &str, colours: &[Colour]) {
+    let codes = colours
+        .iter()
+        .map(|colour| get_colour_code(colour))
+        .collect::<Vec<_>>();
+    let mut outcome = String::new();
+    for code in codes {
+        outcome.push_str(code);
+    }
+
+    outcome += &subject.to_string();
+    println!("{}", outcome);
+}
---
+++ src/todoer/todo.rs
@@ -0,0 +1,32 @@
+#[derive(serde::Deserialize)]
+struct Task {
+    name: String,
+    created_at: u64 
+}
+
+#[derive(serde::Deserialize)]
+struct Todo {
+    tasks: Vec<Task>
+}
+
+impl Todo {
+    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        let mut path = String::new();
+        match std::env::vars().find(|(var, _)| var == "TODORS_CACHE") {
+            None => {
+                path.push_str(".");
+            },
+            Some((_, _path)) => {
+                path = _path;
+            }
+        };
+
+        let tasks = std::fs::read(path + "/todorstasks.json")?;
+        let tasks = String::from_utf8_lossy(&tasks);
+        let tasks = serde_json::from_str::<Vec<Task>>(&tasks)?;
+        Ok(Self {
+            tasks
+        })
+    }
+}
+
